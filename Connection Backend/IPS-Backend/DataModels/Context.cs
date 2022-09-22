da753eeeeaa7bb5ecec7b04245f11e8986c47b8d
Jelmerrr <Jelljel@hotmail.com>
2022-09-22 12:58:11 +0200

    Initial migration

---
+++ Connection Backend/IPS-Backend/DataModels/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPS_Backend.DataModels
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Inventory> Inventories { get; set; }
+        public DbSet<Item> Items { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = mssqlstud.fhict.local; Database = dbi459818_ips3db; User Id = dbi459818_ips3db; Password = Phf0bJTiwt;");
+            //"Server = database,1433; Database = dbi459818_ips3db; User Id = sa; Password = Phf0bJTiwt;" voor docker
+        }
+    }
+}
---
+++ Connection Backend/IPS-Backend/DataModels/Inventory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPS_Backend.DataModels
+{
+    public class Inventory
+    {  
+        public int inventoryID { get; set; }
+        public List<Item> items { get; set; }
+    }
+}
